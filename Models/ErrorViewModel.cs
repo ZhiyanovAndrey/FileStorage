6801f302e1f21c189493327c5dfa09abdfdbc28b
Andrey <aazhiyanov@gmail.com>
2024-09-05 12:35:14 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileStorage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
