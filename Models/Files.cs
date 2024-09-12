598a9ceaab68f9881fdbeba66b7ade11be96ac5f
Andrey <aazhiyanov@gmail.com>
2024-09-12 16:38:35 +0300

    add Files

---
+++ Models/Files.cs
@@ -0,0 +1,13 @@
+﻿namespace FileStorage.Models
+{
+    public class Files
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        public int Size { get; set; }
+        public string Type { get; set; }
+        public int SizeBytes { get; set; }
+
+    }
+}
