0b1ff44ffe428b9ad7f5c9186b88c85c6a93e369
Andrey <aazhiyanov@gmail.com>
2024-09-12 16:01:07 +0300

    add model Folders

---
+++ Controllers/FolderController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileStorage.Controllers
+{
+    public class FolderController : Controller
+    {
+
+        // тестовый запрос
+        [HttpGet("test")]
+        public async Task Test()
+        {
+            Response.ContentType = "text/html;charset=utf-8";
+            await Response.WriteAsync($"Привет! Сервер запущен {DateTime.Now.ToString("D")} в {DateTime.Now.ToString("t")}");
+            string t = $"";
+            //return Ok($"Привет! Сервер запущен {DateTime.Now.ToString("D")} в {DateTime.Now.ToString("t")}");
+        }
+
+    }
+}
---
+++ Models/Folders.cs
@@ -0,0 +1,9 @@
+﻿namespace FileStorage.Models
+{
+    public class Folders
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string IdParentName { get; set; }
+    }
+}
