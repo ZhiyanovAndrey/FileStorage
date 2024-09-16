9c103bdb08ac1c9d53cb5a940de755dd3180dff6
ZhiyanovAndrey <aazhiyanov@gmail.com>
2024-09-16 13:14:35 +0300

    add class Context and  Nget packets

---
+++ Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace FileStorage.Models
+{
+    public class Context
+    {
+        public DbSet<Folders> DbSetFolders { get; set; }   
+        public DbSet<Files> DbSetFiles  { get; set; }   
+        public DbSet<FileExtention> DbSetFileExtentions { get; set; }   
+
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using FileStorage.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// регистрация БД
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+
+
+}
+
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
