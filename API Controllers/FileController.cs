f593be2ffad4619a64d98310bd3609064bf139eb
Andrey <aazhiyanov@gmail.com>
2024-09-25 11:02:16 +0300

    add folder API controllers

---
+++ API Controllers/FileController.cs
@@ -0,0 +1,67 @@
+﻿using FileStorage.Models;
+using FileStorage.Models.Data;
+using FileStorage.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FileStorage.API_Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileService _fileService;
+
+        public FileController(Context db)
+        {
+            _fileService = new FileService(db);
+
+        }
+
+        // GET: api/<FileController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<FileController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FileController>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] FileModel fileModel)
+        {
+            if (fileModel != null)
+            {
+                try
+                {
+                    var result = await _fileService.CreateFileAsync(fileModel);
+                    return  Ok(result);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+            return BadRequest();    
+        }
+
+        // PUT api/<FileController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FileController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
